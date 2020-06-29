1b2a3a98a3087991680eb82a6ea7e931afb79310
unknown <harsharay.ray@gmail.com>
2020-06-29 15:26:53 +0530

    Updates

---
+++ precache-manifest.4b9ec7b12e5360845f1377763e8ec1f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf853a206b9d10bda65394a7973a966",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "ce71b2269d4d5aa9f0ed",
+    "url": "/currency-converter/static/css/main.edf52bab.chunk.css"
+  },
+  {
+    "revision": "7c3ac40720bab9081822",
+    "url": "/currency-converter/static/js/2.10e0a68c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/currency-converter/static/js/2.10e0a68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce71b2269d4d5aa9f0ed",
+    "url": "/currency-converter/static/js/main.e1adc1a1.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
